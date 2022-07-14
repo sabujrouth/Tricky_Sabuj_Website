72ac25148c5af5b68d82aaeac8f1bcb9291d35c9
Sabuj <sabujrouth2000@gmail.com>
2022-07-14 12:37:51 +0530

    Add some section

--- main.js
+++ main.js
@@ -2,5 +2,16 @@
 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 0)
+        ('window-scroll', window.scrollY > 0)
+})
+
+
+// show/hide faq answar
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+    })
 })
